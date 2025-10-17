38d7e1c990559324484359fce479f23d6756707b
AabZap <koontee001@gmail.com>
2025-10-17 19:46:20 +0700

    I kea kong

---
+++ Peck.cs
@@ -0,0 +1,32 @@
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Switch : Stuff , IInteractable
+{
+    public Switch() { 
+        Name = "Switch";
+    }
+    public bool isInteractable { get => CanUse; set => CanUse = value; }
+    [SerializeField]
+    bool isOn = false;
+    Animator animator;
+    public Identity InteracTo;
+    IInteractable IInteract
+    {
+        get
+        {
+            return InteracTo as IInteractable;
+        }
+    }
+
+    public void Interact(Player player)
+    {
+        isOn = !isOn;
+        if (IInteract != null)
+        {
+           IInteract.Interact(player);
+        }
+    }
+}
+
